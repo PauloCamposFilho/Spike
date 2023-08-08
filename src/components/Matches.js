b03e12e748afc05c703f346effe55a953fddbac1
Frank Shen <frank851207@gmail.com>
2023-08-08 14:36:10 -0700

    first front-end commitincluded homepage location matches profile ranking pages

---
+++ src/components/Matches.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+export default function Matches() {
+  return (
+    <div>
+      <Typography variant="h6" component="h2" color="inherit">
+        Matches
+      </Typography>
+    </div>
+  );
+}
