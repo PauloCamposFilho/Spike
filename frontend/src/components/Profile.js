4024b484e6ec5cd92ce56e491362f078e5762dbb
Paulo Campos Filho <pezaum@gmail.com>
2023-08-11 12:53:10 -0700

    Refactor AppBar into SpikeNavBar component

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,27 +1,7 @@
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import SportsVolleyballIcon from "@mui/icons-material/SportsVolleyball";
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  IconButton,
-  Toolbar,
-  Typography,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@material-ui/core";
-import {
-  Stack,
-  Button,
-  ImageList,
-  ImageListItem,
-  Menu,
-  MenuItem,
-  Data,
-  Avatar,
-} from "@mui/material";
+
+import { Typography } from "@material-ui/core";
+import { Stack, Avatar } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -29,6 +9,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import SpikeNavBar from "./AppBar";
 import { UserContext } from "../contexts/userContext";
 function createData(name, value) {
   return { name, value };
@@ -42,119 +23,12 @@ const rows = [
   createData("Updated at", "2023-08-11T02:10:14.175z"),
 ];
 export default function Profile() {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [openMenu, setOpenMenu] = useState(null);
-  const [openLocation, setOpenLocation] = useState(null);
   const { state } = useContext(UserContext);
   const { playerData, matchesData } = state.userData;
 
-  const open = Boolean(anchorEl);
-  const handleClick = (event, setOpen) => {
-    console.log(event);
-    setOpenMenu(false);
-    setOpenLocation(false);
-    setAnchorEl(event.target);
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpenMenu(false);
-    setOpenLocation(false);
-    setAnchorEl(null);
-  };
   return (
     <div style={{display:"flex",justifyContent:"center"}}>
-      <AppBar>
-        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <IconButton
-              size="medium"
-              edge="start"
-              color="inherit"
-              aria-label="logo"
-              component={Link}
-              to="/"
-            >
-              <SportsVolleyballIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="h2"
-              color="inherit"
-              sx={{ marginLeft: "8px" }}
-            >
-              SPIKE!
-            </Typography>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Stack direction="row" spcing={2}>
-              <Button color="inherit" component={Link} to="/profile">
-                Profile
-              </Button>
-              <Button color="inherit" component={Link} to="/matches">
-                Matches
-              </Button>
-              <Button
-                id="location-button"
-                color="inherit"
-                onClick={(event) => handleClick(event, setOpenLocation)}
-                aria-controls={open ? "location-resources" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-              >
-                Location
-              </Button>
-              <Button color="inherit" component={Link} to="ranking">
-                Ranking
-              </Button>
-              {/* we can add authentication after the data retrievment test is done */}
-            </Stack>
-            {anchorEl && (
-              <Menu
-                id="location-resources"
-                anchorEl={anchorEl}
-                open={openLocation}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "location-button",
-                }}
-              >
-                <MenuItem onClick={handleClose} component={Link} to="/location">
-                  Corts
-                </MenuItem>
-                <MenuItem onClick={handleClose} component={Link} to="/location">
-                  Maps
-                </MenuItem>
-              </Menu>
-            )}
-            <IconButton
-              id="nav-menu"
-              style={{ marginRight: "8px" }}
-              color="inherit"
-              onClick={(event) => handleClick(event, setOpenMenu)}
-              aria-controls={open ? "menu-resources" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <MenuIcon />
-            </IconButton>
-            {anchorEl && (
-              <Menu
-                id="menu-resources"
-                anchorEl={anchorEl}
-                open={openMenu}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby2": "nav-menu",
-                }}
-              >
-                <MenuItem onClick={handleClose} component={Link} to="/login">
-                  Login 123
-                </MenuItem>
-              </Menu>
-            )}
-          </div>
-        </Toolbar>
-      </AppBar>
+      <SpikeNavBar/>
       <div style={{ padding: "80px"}}>
         <Typography variant="h4" component="h2" color="inherit" paddingbottom="10px">
           Player Profiles
