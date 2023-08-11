6c12f9345587bd56f34734f220e149ec0aa3f0a5
Frank Shen <frank851207@gmail.com>
2023-08-11 15:12:30 -0700

    Insert the QR code in the profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -31,7 +31,8 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { UserContext } from "../contexts/userContext";
-import "../style/Profile.css"
+import { QRCodeGenerator } from "./QRcode";
+import "../style/Profile.css";
 
 export default function Profile() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -66,201 +67,296 @@ export default function Profile() {
   };
 
   return (
-    <div style={{display:"flex",justifyContent:"center"}}>
-      <AppBar>
-        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <IconButton
-              size="medium"
-              edge="start"
-              color="inherit"
-              aria-label="logo"
-              component={Link}
-              to="/"
-            >
-              <SportsVolleyballIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="h2"
-              color="inherit"
-              sx={{ marginLeft: "8px" }}
-            >
-              SPIKE!
-            </Typography>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Stack direction="row" spcing={2}>
-              <Button color="inherit" component={Link} to="/profile">
-                Profile
-              </Button>
-              <Button color="inherit" component={Link} to="/matches">
-                Matches
-              </Button>
-              <Button
-                id="location-button"
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        <AppBar>
+          <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <IconButton
+                size="medium"
+                edge="start"
                 color="inherit"
-                onClick={(event) => handleClick(event, setOpenLocation)}
-                aria-controls={open ? "location-resources" : undefined}
+                aria-label="logo"
+                component={Link}
+                to="/"
+              >
+                <SportsVolleyballIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="h2"
+                color="inherit"
+                sx={{ marginLeft: "8px" }}
+              >
+                SPIKE!
+              </Typography>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Stack direction="row" spcing={2}>
+                <Button color="inherit" component={Link} to="/profile">
+                  Profile
+                </Button>
+                <Button color="inherit" component={Link} to="/matches">
+                  Matches
+                </Button>
+                <Button
+                  id="location-button"
+                  color="inherit"
+                  onClick={(event) => handleClick(event, setOpenLocation)}
+                  aria-controls={open ? "location-resources" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                >
+                  Location
+                </Button>
+                <Button color="inherit" component={Link} to="ranking">
+                  Ranking
+                </Button>
+                {/* we can add authentication after the data retrievment test is done */}
+              </Stack>
+              {anchorEl && (
+                <Menu
+                  id="location-resources"
+                  anchorEl={anchorEl}
+                  open={openLocation}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "location-button",
+                  }}
+                >
+                  <MenuItem
+                    onClick={handleClose}
+                    component={Link}
+                    to="/location"
+                  >
+                    Corts
+                  </MenuItem>
+                  <MenuItem
+                    onClick={handleClose}
+                    component={Link}
+                    to="/location"
+                  >
+                    Maps
+                  </MenuItem>
+                </Menu>
+              )}
+              <IconButton
+                id="nav-menu"
+                style={{ marginRight: "8px" }}
+                color="inherit"
+                onClick={(event) => handleClick(event, setOpenMenu)}
+                aria-controls={open ? "menu-resources" : undefined}
                 aria-haspopup="true"
                 aria-expanded={open ? "true" : undefined}
               >
-                Location
-              </Button>
-              <Button color="inherit" component={Link} to="ranking">
-                Ranking
-              </Button>
-              {/* we can add authentication after the data retrievment test is done */}
-            </Stack>
-            {anchorEl && (
-              <Menu
-                id="location-resources"
-                anchorEl={anchorEl}
-                open={openLocation}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "location-button",
-                }}
-              >
-                <MenuItem onClick={handleClose} component={Link} to="/location">
-                  Corts
-                </MenuItem>
-                <MenuItem onClick={handleClose} component={Link} to="/location">
-                  Maps
-                </MenuItem>
-              </Menu>
-            )}
-            <IconButton
-              id="nav-menu"
-              style={{ marginRight: "8px" }}
-              color="inherit"
-              onClick={(event) => handleClick(event, setOpenMenu)}
-              aria-controls={open ? "menu-resources" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <MenuIcon />
-            </IconButton>
-            {anchorEl && (
-              <Menu
-                id="menu-resources"
-                anchorEl={anchorEl}
-                open={openMenu}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby2": "nav-menu",
-                }}
-              >
-                <MenuItem onClick={handleClose} component={Link} to="/login">
-                  Login 123
-                </MenuItem>
-              </Menu>
-            )}
-          </div>
-        </Toolbar>
-      </AppBar>
-      <div style={{ padding: "80px"}}>
-        <Typography variant="h4" component="h2" color="inherit" paddingbottom="10px">
-          Player Profiles
-        </Typography>
-        <div
-          style={{
-            direction: "row",
-            display: "flex",
-            height: "800px",
-            paddingTop: "40px"
-          }}
-        >
-          {/* use border: "1px solid red" for the frame of <div> or different element */}
+                <MenuIcon />
+              </IconButton>
+              {anchorEl && (
+                <Menu
+                  id="menu-resources"
+                  anchorEl={anchorEl}
+                  open={openMenu}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby2": "nav-menu",
+                  }}
+                >
+                  <MenuItem onClick={handleClose} component={Link} to="/login">
+                    Login 123
+                  </MenuItem>
+                </Menu>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div style={{ padding: "80px" }}>
+          <Typography
+            variant="h4"
+            component="h2"
+            color="inherit"
+            paddingbottom="10px"
+          >
+            Player Profiles
+          </Typography>
           <div
             style={{
-              margin:"30px"
+              direction: "row",
+              display: "flex",
+              height: "800px",
+              paddingTop: "40px",
             }}
           >
-            <Stack direction="row" spacing={2}>
-              <Avatar
-                sx={{ width: 200, height: 200 }}
-                alt="Remy Sharp"
-                // src="https://i.pinimg.com/736x/33/06/b8/3306b8156653fea183b5406151c74ded.jpg"
-                src={playerData.avatar_picture}
-              />
-            </Stack>
-          </div>
-          <div className="table-section">
-            <TableContainer component={Paper}>
-              <Table style={{ width: "700px" }} aria-label="simple table">
-                <TableHead className="table-header">
-                  <TableRow >
-                    <TableCell>First Name</TableCell>
-                    <TableCell>Last Name</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow className="table-row" key="name" sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell component="th" scope="row">
-                      {playerData.first_name}
-                    </TableCell>
-                    <TableCell align="left">{playerData.last_name}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TableContainer component={Paper} style={ {"margin-top": "30px"} }>
-              <Table style={{ width: "700px" }} aria-label="simple table">
-                  <TableHead>
-                  <TableRow key="elo" sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell component="th" scope="row" ><h3 style={{color: getEloRatingColor(), alignItems: "center", justifyContent: "center", display: "flex"}}>Current Elo Rating: { playerData.elo_rating}</h3></TableCell>
-                  </TableRow>
-                  </TableHead>
-                </Table>
-            </TableContainer>
-
-            <TableContainer component={Paper} style={ {"margin-top": "30px"} }>
-              <Table style={{ width: "700px" }} aria-label="simple table">
+            {/* use border: "1px solid red" for the frame of <div> or different element */}
+            <div
+              style={{
+                margin: "30px",
+              }}
+            >
+              <Stack direction="row" spacing={2}>
+                <Avatar
+                  sx={{ width: 200, height: 200 }}
+                  alt="Remy Sharp"
+                  // src="https://i.pinimg.com/736x/33/06/b8/3306b8156653fea183b5406151c74ded.jpg"
+                  src={playerData.avatar_picture}
+                />
+              </Stack>
+            </div>
+            <div className="table-section">
+              <TableContainer component={Paper}>
+                <Table style={{ width: "700px" }} aria-label="simple table">
                   <TableHead className="table-header">
                     <TableRow>
-                      <TableCell colSpan={5} align="center">Description</TableCell>
+                      <TableCell>First Name</TableCell>
+                      <TableCell>Last Name</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    <TableRow key="bio" sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                        <TableCell className="table-row" colSpan={5} component="th" scope="row">
-                          {playerData.description}
-                        </TableCell>
-                      </TableRow>
+                    <TableRow
+                      className="table-row"
+                      key="name"
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {playerData.first_name}
+                      </TableCell>
+                      <TableCell align="left">{playerData.last_name}</TableCell>
+                    </TableRow>
                   </TableBody>
                 </Table>
-            </TableContainer>
+              </TableContainer>
+              <TableContainer
+                component={Paper}
+                style={{ "margin-top": "30px" }}
+              >
+                <Table style={{ width: "700px" }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow
+                      key="elo"
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <h3
+                          style={{
+                            color: getEloRatingColor(),
+                            alignItems: "center",
+                            justifyContent: "center",
+                            display: "flex",
+                          }}
+                        >
+                          Current Elo Rating: {playerData.elo_rating}
+                        </h3>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              </TableContainer>
 
-            <TableContainer component={Paper} style={ { "margin-top": "30px" } }>
-              <Table style={{ width: "700px" }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell colSpan={5} align="center"><h2>Recent Matches</h2></TableCell>
-                  </TableRow>
-                  <TableRow className="table-header">
-                    <TableCell  style={{"width": "110px"}}  align="left"><h4>Team</h4></TableCell>
-                    <TableCell><h4></h4></TableCell>
-                    <TableCell style={{ "width": "110px"}} align="left"><h4>Team</h4></TableCell>
-                    <TableCell align="left"><h4>Location</h4></TableCell>
-                    <TableCell align="left"><h4>Date</h4></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody >
+              <TableContainer
+                component={Paper}
+                style={{ "margin-top": "30px" }}
+              >
+                <Table style={{ width: "700px" }} aria-label="simple table">
+                  <TableHead className="table-header">
+                    <TableRow>
+                      <TableCell colSpan={5} align="center">
+                        Description
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow
+                      key="bio"
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell
+                        className="table-row"
+                        colSpan={5}
+                        component="th"
+                        scope="row"
+                      >
+                        {playerData.description}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+
+              <TableContainer
+                component={Paper}
+                style={{ "margin-top": "30px", border: "2px solid green" }}
+              >
+                <Table style={{ width: "700px" }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell colSpan={5} align="center">
+                        <h2>Recent Matches</h2>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow className="table-header">
+                      <TableCell style={{ width: "110px" }} align="left">
+                        <h4>Team</h4>
+                      </TableCell>
+                      <TableCell>
+                        <h4></h4>
+                      </TableCell>
+                      <TableCell style={{ width: "110px" }} align="left">
+                        <h4>Team</h4>
+                      </TableCell>
+                      <TableCell align="left">
+                        <h4>Location</h4>
+                      </TableCell>
+                      <TableCell align="left">
+                        <h4>Date</h4>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
                     {matchesData.map((item, index) => {
                       return (
-                        <TableRow className="table-row" key={item.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                          <TableCell align="left">{item.winner_team_name}</TableCell>
-                          <TableCell><strong>defeats</strong></TableCell>
-                          <TableCell align="left">{item.other_team_name}</TableCell>
-                          <TableCell align="left">{item.play_area_name}</TableCell>
+                        <TableRow
+                          className="table-row"
+                          key={item.id}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell align="left">
+                            {item.winner_team_name}
+                          </TableCell>
+                          <TableCell>
+                            <strong>defeats</strong>
+                          </TableCell>
+                          <TableCell align="left">
+                            {item.other_team_name}
+                          </TableCell>
+                          <TableCell align="left">
+                            {item.play_area_name}
+                          </TableCell>
                           <TableCell align="left">{item.created_at}</TableCell>
                         </TableRow>
-                      )
+                      );
                     })}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  padding: "16px",
+                  border: "2px solid yellow",
+                }}
+              >
+                <IconButton
+                  size="medium"
+                  edge="start"
+                  color="inherit"
+                  aria-label="logo"
+                >
+                  <QRCodeGenerator />
+                </IconButton>
+              </div>
+            </div>
           </div>
         </div>
       </div>
