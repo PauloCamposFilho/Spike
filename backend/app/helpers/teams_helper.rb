9d9dc36583f2fe24963e3d5c28c716d489446320
Paulo Campos Filho <pezaum@gmail.com>
2023-08-12 21:11:43 -0700

    Add fields to JSON return

--- backend/app/helpers/teams_helper.rb
+++ backend/app/helpers/teams_helper.rb
@@ -2,10 +2,14 @@ module TeamsHelper
   private
 
   def add_play_area_name(matches)
-    matches.includes(:play_area).map do |match|
+    matches.includes(:play_area, match_rosters: { player: :teams }).map do |match|
+      winner_team_average_elo = match.winner_team.average_elo_rating
+      other_team_average_elo = match.other_team.average_elo_rating
+
       match.as_json.merge(
-        play_area_name: match.play_area.name,
-        play_area_id: match.play_area.id
+        play_area: match.play_area,
+        winner_team: match.winner_team.as_json.merge(average_elo_rating: winner_team_average_elo),
+        other_team: match.other_team.as_json.merge(average_elo_rating: other_team_average_elo)
       )
     end
   end
