de591a5e0d96e13a00877aac525720eba3e37ccc
Paulo Campos Filho <pezaum@gmail.com>
2023-08-12 23:02:31 -0700

    Fix initialState. Fix API url relative path string

--- frontend/src/helpers/fetchCurrentUserData.js
+++ frontend/src/helpers/fetchCurrentUserData.js
@@ -1,13 +1,13 @@
 const fetchCurrentUserData = async (playerId) => {
   try {
     // Fetch data
-    const playerRes = await fetch(`api/players/${playerId}`);
-    const matchesRes = await fetch(`api/players/${playerId}/matches`);
-    const teamsRes = await fetch(`api/players/${playerId}/teams`);
-    const teamsMatchesRes = await fetch(`api/players/${playerId}/teams/matches`);
-    const playAreaRes = await fetch(`api/players/${playerId}/playarea/favorite`);
-    const playerRankingsRes = await fetch(`api/players/rankings`);
-    const teamRankingsRes = await fetch(`api/teams/rankings`);
+    const playerRes = await fetch(`/api/players/${playerId}`);
+    const matchesRes = await fetch(`/api/players/${playerId}/matches`);
+    const teamsRes = await fetch(`/api/players/${playerId}/teams`);
+    const teamsMatchesRes = await fetch(`/api/players/${playerId}/teams/matches`);
+    const playAreaRes = await fetch(`/api/players/${playerId}/playarea/favorite`);
+    const playerRankingsRes = await fetch(`/api/players/rankings`);
+    const teamRankingsRes = await fetch(`/api/teams/rankings`);
     // Parse JSON
     const playerData = await playerRes.json();
     const matchesData = await matchesRes.json();
