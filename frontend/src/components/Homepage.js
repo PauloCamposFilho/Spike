5caada8f81297066bca617c2cd8e2f6a4dad14c6
teksts <mark.egelhoff1@gmail.com>
2023-08-14 16:38:01 -0700

    Style Home Court and Team widgets

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -23,13 +23,14 @@ import PlusButton from "./HomepagePlusButton";
 import PlusButtonModal from "./PlusButtonModal";
 import { Stack, LinearProgress } from "@mui/material";
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import HandshakeRoundedIcon from '@mui/icons-material/HandshakeRounded';
 import { useTheme } from "@emotion/react";
 import '../style/fonts.scss'
 
 
 export default function Homepage() {
   const { state } = useContext(UserContext);
-  const { isLoading } = state.userData.teamsData;
+  // const { isLoading } = state.userData.isLoading;
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -40,83 +41,88 @@ export default function Homepage() {
   return (
     <div className="home-page">
       <SpikeNavBar />
-      {/* the iframe src hasn't put into the right api to render the google map */}
-
-      <div
-        style={{
-          // border:"2px solid yellow",
-          width: "100%",
-          height: "50%",
-          marginTop: "64px",
-          display: "flex",
-          flexDirection: "row",
-          padding: "40px",
-          gap: "80px",
-        }}
-      >
-        <Box style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          padding: "1%",
-          alignItems: "center",
-        }} component={Paper} elevation={2}>
-          <Grid container style={{ width: "90%" }} spacing={0}>
-            <Grid item container xs={12}>
-              <Grid item xs={12}>
-                <Typography variant={"h4"} style={{ color: '#7b7b7b' }}>
-                  <HomeRoundedIcon fontSize="medium" /> Home Court
+      <Grid container spacing={10} style={{
+        marginTop: "64px",
+        flexDirection: "column",
+        flexWrap: 'wrap',
+        alignItems: 'center',
+      }}>
+        <Grid container item xs={12} style={{ justifyContent: 'space-evenly' }}>
+          <Grid container item xs={5} style={{
+            flexDirection: "column",
+            padding: "1%",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }} component={Paper} elevation={2}>
+            <Grid container item style={{ width: "90%" }} spacing={0}>
+              <Grid item container xs={12}>
+                <Grid item xs={12}>
+                  <Typography variant={"h4"} style={{ color: '#7b7b7b' }}>
+                    <HomeRoundedIcon fontSize="medium" /> Home Court
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item container xs={12} spacing={2}><Grid item xs={12}>
+                <Typography variant={"h3"}>
+                  Rogers Arena
+                  {/* {state.userData.playAreaData.play_area.name} */}
                 </Typography>
               </Grid>
-            </Grid>
-            <Grid item container xs={12} spacing={2}><Grid item xs={12}>
-              <Typography variant={"h3"}>
-                Rogers Arena
-                {/* {state.userData.playAreaData.play_area.name} */}
-              </Typography>
-            </Grid>
-              <Grid item xs={12} style={{ height: "75%" }}>
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.332284251036!2d-122.94344676087555!3d49.289048300000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548679f393721d0f%3A0x361aacc5ca2c032d!2sVolleyball%20BC!5e0!3m2!1sen!2sca!4v1691617114859!5m2!1sen!2sca"
-                  style={{ border: 0, height: "100%", width: "100%", borderRadius: "5px", }}
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                  title="googlemap-volleyball-court"
-                ></iframe>
-              </Grid>
-              <Grid container item xs={12} spacing={0} style={{ padding: 0 }}>
-                <Grid item xs={10} alignItems="flex-end">
-                  <Typography variant="caption">Activity Level</Typography>
+                <Grid item xs={12} style={{ height: "70%" }}>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.332284251036!2d-122.94344676087555!3d49.289048300000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548679f393721d0f%3A0x361aacc5ca2c032d!2sVolleyball%20BC!5e0!3m2!1sen!2sca!4v1691617114859!5m2!1sen!2sca"
+                    style={{ border: 0, height: "100%", width: "100%", borderRadius: "5px", }}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="googlemap-volleyball-court"
+                  ></iframe>
                 </Grid>
-                <Grid container item xs={12} alignItems="center" spacing={2} style={{ padding: 0 }}>
-                  <Grid item xs={10}>
-                    <LinearProgress style={{ height: 10, borderRadius: 5, border: 'solid', background: theme.palette.primary.light }} color='primary' variant="determinate" value={80} />
+                <Grid container item xs={12} spacing={0} style={{ padding: 0 }}>
+                  <Grid item xs={10} alignItems="flex-end">
+                    <Typography variant="caption">Activity Level</Typography>
                   </Grid>
-                  <Grid item xs={2} style={{ padding: 0 }}>
-                    <Typography style={{ fontFamily: 'Bangers', fontSize: "1.4rem" }}>Jammin!!</Typography>
+                  <Grid container item xs={12} alignItems="center" spacing={2} style={{ padding: 0 }}>
+                    <Grid item xs={10}>
+                      <LinearProgress style={{ height: 10, borderRadius: 5, border: 'solid', background: theme.palette.primary.light }} color='primary' variant="determinate" value={80} />
+                    </Grid>
+                    <Grid item xs={2} style={{ padding: 0 }}>
+                      <Typography style={{ fontFamily: 'Bangers', fontSize: "1.4rem" }}>Jammin!!</Typography>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Box>
-        <div style={{ border: "1px solid green", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          < WidgetListOfTeams />
-        </div>
-      </div>
-      <div style={{ border: "1px solid green", width: "100%", minHeight: 100, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-        {!isLoading &&
-          <>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <Grid container item xs={5} component={Paper} elevation={2} style={{
+            flexDirection: "column",
+            padding: "1%",
+            alignItems: "center",
+            flexWrap: "wrap"
+          }}>
+            <Grid container item style={{ width: "90%" }} spacing={0}>
+              <Grid container item xs={12}>
+                <Grid item xs={12}>
+                  <Typography variant={"h4"} style={{ color: '#7b7b7b' }}>
+                    <HandshakeRoundedIcon fontSize="medium" /> Teams
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <WidgetListOfTeams />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container item xs={10} component={Paper} elevation={2} style={{ minWidth: '60%' }}>
+          <Grid item xs={12}>
+            <TableContainer>
+              <Table aria-label="a dense table">
                 <TableHead>
                   <TableRow>
                     <TableCell></TableCell>
                     <TableCell align="right"></TableCell>
                     <TableCell></TableCell>
                     <TableCell align="right">Date</TableCell>
-                    {/* <TableCell align="right">Result</TableCell>
-                  <TableCell align="right">Date</TableCell> */}
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -130,13 +136,15 @@ export default function Homepage() {
                       <TableCell align="right">{row.other_team_name}</TableCell>
                       <TableCell align="right">{row.created_at}</TableCell>
                       {/* <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell> */}
+                          <TableCell align="right">{row.protein}</TableCell> */}
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
             </TableContainer>
-            <div
+          </Grid>
+        </Grid>
+        {/* <div
               style={{
                 display: "flex",
                 width: "100%",
@@ -146,17 +154,12 @@ export default function Homepage() {
             >
               <PlusButton handleOpen={handleOpen} />
 
-            </div>
-          </>
-        }
-        {isLoading && <CircularProgress />}
-      </div>
-
+            </div> */}
+      </Grid>
       <PlusButtonModal
         open={open}
         onClose={handleClose}
       />
-
     </div>
   );
 }
