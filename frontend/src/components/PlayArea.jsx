0df29d067ff4dbd98c3957e17ceaa730b8db2a4c
Frank Shen <frank851207@gmail.com>
2023-08-15 15:28:37 -0700

    small bug fix

--- frontend/src/components/PlayArea.jsx
+++ frontend/src/components/PlayArea.jsx
@@ -75,7 +75,7 @@ export default function PlayArea(props) {
         </Typography>
         <TableRow
           key={id}
-          width="100%"
+          style={{width:"100%"}}
           sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
         >
           <TableCell align="left">
